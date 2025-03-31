a89eca8264bf3c93a1308ecae277059be4e8c816
Subrajit Jena <128827071+sjjena@users.noreply.github.com>
2025-03-31 13:09:08 +0530

    Add files via upload

---
+++ Basic_math_operation.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+
+print('Addition:',c)
+print('Substraction:',d)
+print('Multiplication:',e)
+print('Division:',f)
